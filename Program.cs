bb4a9f352947218548a167e65ce727e986a22024
Marko21552 <98662057+Marko21552@users.noreply.github.com>
2022-08-24 08:59:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to .OS 1.0!");
+            bool done = false;
+            while (!done)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "ver")
+                {
+                    Console.WriteLine(".OS v0.1");
+                    Console.WriteLine("Made by Marko2155");
+                }
+
+                if (input == "help")
+                {
+                    Console.WriteLine("help - The command that shows this menu.");
+                    Console.WriteLine("ver - Shows .OS version");
+                    Console.WriteLine("shutdown - Shuts down the operating system");
+                } else
+                {
+                    Console.WriteLine("Command not recognised.");
+                }
+
+                if (input == "shutdown")
+                {
+                    Environment.Exit(1);
+                }
+            }
+        }
+    }
+}
